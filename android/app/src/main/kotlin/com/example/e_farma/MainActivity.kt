912bd2e35446fe93725903a97064d2650f7267c9
wagner coche <wagnercoche@gmail.com>
2022-11-28 16:56:11 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/e_farma/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.e_farma
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'app/app_module.dart';
+import 'app/app_widget.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  runApp(
+    ModularApp(
+      module: AppModule(), 
+      child: AppWidget()
+    )
+  );
+}
